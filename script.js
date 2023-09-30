26bec8663ebfef198b6afc0ea4ff843277a19a84
Manish Jha <104479521+manish189000@users.noreply.github.com>
2023-09-30 18:44:51 +0530

    uploded

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-alert("manish");
+function submitbutton() {
+                    
+    const selectedCheckbox = document.querySelector('input[name="socialMedia"]:checked');
+
+  
+    const displayInput = document.getElementById('displayInput');
+    displayInput.value = selectedCheckbox ? selectedCheckbox.value : '';
+
+   
+    if (selectedCheckbox) {
+        const link = selectedCheckbox.getAttribute('data-link');
+        window.open(link, '_blank');
+    }
+}
